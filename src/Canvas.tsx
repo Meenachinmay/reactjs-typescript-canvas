a1651fec062b94b0f49683fd2d2697e7d076da27
meenachinmay <chinmayanand896@gmail.com>
2023-02-27 01:57:08 +0530

    drag and drop multiple shapes using reactjs and HTML 5 canvas

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,77 +1,75 @@
 import React, { useRef, useEffect, useState } from "react";
-import Square from "./Shapes/Square";
 
-const Canvas = () => {
+interface Square {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  dragging: boolean;
+  color: string;
+}
+
+const Canvas: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [squarePosition, setSquarePosition] = useState({ x: 50, y: 50 });
-  const [isDragging, setIsDragging] = useState(false);
+  const [squares, setSquares] = useState<Square[]>([]);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas!.getContext("2d")!;
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
 
-    const sq = new Square();
+      const mouseDownHandler = (event: MouseEvent) => {
+        const { offsetX, offsetY } = event;
+        const square = squares.find(
+          (square) =>
+            offsetX >= square.x &&
+            offsetX <= square.x + square.width &&
+            offsetY >= square.y &&
+            offsetY <= square.y + square.height
+        );
+        if (square) {
+          square.dragging = true;
+        }
+      };
 
-    const drawSquare = () => {
-      ctx.clearRect(0, 0, canvas!.width, canvas!.height);
-      sq.createSquare(ctx, "blue", squarePosition.x, squarePosition.y, 50, 50);
-      sq.createSquare(ctx, "red", squarePosition.x + 40, squarePosition.y + 40, 50, 50);
-    };
+      const mouseMoveHandler = (event: MouseEvent) => {
+        const { offsetX, offsetY } = event;
+        const draggingSquare = squares.find((square) => square.dragging);
+        if (draggingSquare) {
+          draggingSquare.x = offsetX - draggingSquare.width / 2;
+          draggingSquare.y = offsetY - draggingSquare.height / 2;
+          draw();
+        }
+      };
 
-    drawSquare();
-  }, [squarePosition]);
+      const mouseUpHandler = () => {
+        squares.forEach((square) => (square.dragging = false));
+      };
 
-  const handleMouseDown = (event: React.MouseEvent<HTMLCanvasElement>) => {
-    const canvas = canvasRef.current!;
-    const rect = canvas.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
+      const draw = () => {
+        ctx!.clearRect(0, 0, canvas.width, canvas.height);
+        squares.forEach((square) => {    
+          ctx!.fillRect(square.x, square.y, square.width, square.height);
+        });
+      };
 
-    if (
-      x >= squarePosition.x &&
-      x <= squarePosition.x + 50 &&
-      y >= squarePosition.y &&
-      y <= squarePosition.y + 50
-    ) {
-      setIsDragging(true);
+      canvas.addEventListener("mousedown", mouseDownHandler);
+      canvas.addEventListener("mousemove", mouseMoveHandler);
+      canvas.addEventListener("mouseup", mouseUpHandler);
+
+      setSquares([
+        { x: 50, y: 50, width: 50, height: 50, dragging: false, color: "red" },
+        { x: 150, y: 50, width: 50, height: 50, dragging: false, color: "blue" },
+        { x: 250, y: 50, width: 50, height: 50, dragging: false, color: "green" },
+        { x: 350, y: 50, width: 50, height: 50, dragging: false, color: "black" },
+        { x: 450, y: 50, width: 50, height: 50, dragging: false, color: "orange" },
+      ]);
+
+      draw();
     }
-  };
+  }, []);
 
-  const handleMouseMove = (event: React.MouseEvent<HTMLCanvasElement>) => {
-    if (isDragging) {
-      const canvas = canvasRef.current!;
-      const rect = canvas.getBoundingClientRect();
-      const x = event.clientX - rect.left - 25;
-      const y = event.clientY - rect.top - 25;
-      setSquarePosition({ x, y });
-    }
-  };
-
-  const handleMouseUp = () => {
-    setIsDragging(false);
-  };
-
-  return (
-    <>
-      <div
-        style={{
-          border: "1px solid",
-          borderRadius: "5px",
-          maxWidth: "500px",
-          maxHeight: "500px",
-        }}
-      >
-        <canvas
-          ref={canvasRef}
-          width={500}
-          height={500}
-          onMouseDown={handleMouseDown}
-          onMouseMove={handleMouseMove}
-          onMouseUp={handleMouseUp}
-        ></canvas>
-      </div>
-    </>
-  );
+  return <canvas ref={canvasRef} width={500} height={500} />;
 };
 
 export default Canvas;
--- src/Shapes/Square.ts
+++
@@ -1,9 +0,0 @@
-class Square {
-  createSquare(context: CanvasRenderingContext2D, color: string, x:number, y: number, 
-    width: number, height: number) {
-    context.fillStyle = color;
-    context.fillRect(x, y, width, height);
-  }
-}
-
-export default Square;
